ddbb05c8c2692d5d7883a52d7fa28891a731d86c
Nada Hasni <nada.hasni@edi-indonesia.co.id>
2025-03-25 17:09:47 +0700

    feat: add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,75 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vitePluginBundleObfuscator from "vite-plugin-bundle-obfuscator";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    vitePluginBundleObfuscator({
+      enable: true,
+      options: {
+        simplify: true,
+        compact: true,
+      },
+    }),
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "android-chrome-192x192.png",
+        "android-chrome-512x512.png",
+        "favicon-16x16.png",
+        "favicon-32x32.png",
+      ],
+      manifest: {
+        name: "Bjirlah Men",
+        short_name: "Bjir",
+        description: "Bjirlah men jangan lupa absen",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon",
+          },
+          {
+            src: "/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#FFFFFF",
+        background_color: "#209CEE",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
